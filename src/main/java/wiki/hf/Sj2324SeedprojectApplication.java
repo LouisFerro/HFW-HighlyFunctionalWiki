3ddcb1ca3e50b4365c635a877d74a6f6deca1bd4
Louis Ferro <FER22362@spengergasse.at>
2023-12-15 19:25:29 +0100

    Domain refactored to wiki.hf.

--- src/main/java/wiki/hf/Sj2324SeedprojectApplication.java
+++ src/main/java/wiki/hf/Sj2324SeedprojectApplication.java
@@ -1,4 +1,4 @@
-package at.spengergasse.sj2324seedproject;
+package wiki.hf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/wiki/hf/Sj2324SeedprojectApplicationTests.java
+++ src/test/java/wiki/hf/Sj2324SeedprojectApplicationTests.java
@@ -1,4 +1,4 @@
-package at.spengergasse.sj2324seedproject;
+package wiki.hf;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
