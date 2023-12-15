a540b6ec2b37aa4f7766294fdedc0ba80c8bbf00
Louis Ferro <FER22362@spengergasse.at>
2023-12-15 23:09:48 +0100

    Test creation for saving and reading an account.

--- src/test/java/wiki/hf/persistence/AccountRepositoryTest.java
+++ src/test/java/wiki/hf/persistence/AccountRepositoryTest.java
@@ -1,8 +1,31 @@
 package wiki.hf.persistence;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import wiki.hf.domain.Account;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class AccountRepositoryTest
 {
+    @Autowired
+    private AccountRepository repository;
 
+    @Test
+    void SavingAndReadingAnAccountTest()
+    {
+        var account = Account.builder()
+                             .fullName("Louis Ferro")
+                             .username("Louisthemagic")
+                             .password("password")
+                             .build();
+
+        var save = repository.save(account);
+
+        assertThat(save).isNotNull().isSameAs(account);
+        assertThat(save.getId()).isNotNull();
+    }
 }
