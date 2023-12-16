4ef0cae12b6d95d638ce11f3980ed60945294256
Louis Ferro <FER22362@spengergasse.at>
2023-12-16 16:26:46 +0100

    Removement of unnecessary import.

--- src/test/java/wiki/hf/persistence/AccountRepositoryTest.java
+++ src/test/java/wiki/hf/persistence/AccountRepositoryTest.java
@@ -2,13 +2,14 @@ package wiki.hf.persistence;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import wiki.hf.domain.Account;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.ANY)
 class AccountRepositoryTest
 {
     @Autowired
