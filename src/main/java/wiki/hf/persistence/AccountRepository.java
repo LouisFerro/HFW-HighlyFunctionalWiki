7aa8b3698fd0e51c4bdc03902e0ea8320ec6d36f
Louis Ferro <FER22362@spengergasse.at>
2023-12-17 15:28:31 +0100

    Disablement of the unnecessary Repositories AccountRepository.java and CommentRepository.java.

--- src/main/java/wiki/hf/persistence/AccountRepository.java
+++ src/main/java/wiki/hf/persistence/AccountRepository.java
@@ -1,9 +1,11 @@
 /* TODO: Implement.
+
 package wiki.hf.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import wiki.hf.domain.Account;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long>
 {
 
--- src/main/java/wiki/hf/persistence/CommentRepository.java
+++ src/main/java/wiki/hf/persistence/CommentRepository.java
@@ -5,6 +5,7 @@ package wiki.hf.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import wiki.hf.domain.Comment;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long>
 {
 
