b96d9a550605f85ec3634d10d8c917d55ea01663
Louis Ferro <FER22362@spengergasse.at>
2023-12-19 16:29:25 +0100

    Refactoring of Content which no longer inherits from Metadata.java for the sake of necessity.

--- src/main/java/wiki/hf/domain/Content.java
+++ src/main/java/wiki/hf/domain/Content.java
@@ -5,22 +5,16 @@ package wiki.hf.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.nio.Buffer;
+//import java.nio.Buffer;
 
 @Data
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@AllArgsConstructor
 
 @Embeddable
-public class Content extends Metadata
+public class Content
 {
     private String text;
     private String image;
-
-    @Builder
-    public Content(String name, String description, Change change, String text, String image)
-    {
-        super(name, description, change);
-        this.text = text;
-        this.image = image;
-    }
+    //Private Buffer image
 }
