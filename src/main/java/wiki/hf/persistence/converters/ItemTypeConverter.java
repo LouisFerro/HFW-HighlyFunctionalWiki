f89f300652bfd2509da0dd3f5f29ad1a9ffddab8
Louis Ferro <FER22362@spengergasse.at>
2023-12-19 18:12:43 +0100

    Implementation of ItemTypeConverter.java.

---
+++ src/main/java/wiki/hf/persistence/converters/ItemTypeConverter.java
@@ -0,0 +1,38 @@
+package wiki.hf.persistence.converters;
+
+import wiki.hf.domain.*;
+import static wiki.hf.domain.ItemType.*;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.*;
+
+@Converter
+public class ItemTypeConverter implements AttributeConverter<ItemType, String>
+{
+    @Override
+    public String convertToDatabaseColumn(ItemType itemType)
+    {
+        return Optional.ofNullable(itemType)
+                       .map(type -> switch (type) {
+                            case LIST -> "L";
+                            case TEXT -> "T";
+                            case IMAGE -> "I";
+                            case VIDEO -> "V";
+                       }).orElse(null);
+    }
+
+    @Override
+    public ItemType convertToEntityAttribute(String value)
+    {
+        return Optional.ofNullable(value)
+                       .map(v -> switch (v) {
+                           case "L" -> LIST;
+                           case "T" -> TEXT;
+                           case "I" -> IMAGE;
+                           case "V" -> VIDEO;
+                           default -> throw new IllegalArgumentException("\"%s\" is not a valid value for Enumerator ItemType".formatted(v));
+                       }).orElse(null);
+    }
+}
