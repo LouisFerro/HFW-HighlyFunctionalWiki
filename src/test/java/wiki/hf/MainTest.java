a7b07dbfab08125355d0a134918e76a19d772dae
Louis Ferro <FER22362@spengergasse.at>
2024-01-20 15:35:11 +0100

    Refactoring of container configuration to have port binding and other properly set up.

---
+++ src/test/java/wiki/hf/MainTest.java
@@ -0,0 +1,38 @@
+package wiki.hf;
+
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.HostConfig;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.api.model.Ports;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@Configuration
+public class MainTest {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        final int localPort = 65432;
+        final int exposedPort = 5432;
+
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:15-bullseye"))
+                                         .withCreateContainerCmdModifier(cmd -> {
+                                             cmd.withName("4EHIF-POS-Postgres");
+                                             cmd.withHostConfig(
+                                                 new HostConfig().withPortBindings(
+                                                     new PortBinding(Ports.Binding.bindPort(localPort), new ExposedPort(exposedPort))));
+                                         })
+                                         .withReuse(true);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(Application::main)
+                .with(MainTest.class)
+                .run(args);
+    }
+}
